c19c890a6591652c11fb030ea5df5303d436bc5f
EvanOLeary26 <evan.j.oleary@gmail.com>
2025-04-23 16:15:09 -0400

    added merge sort, other small changes

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <chrono>
+#include <cstdlib>
+#include <ctime>
 
 #include "SortingAlgorithms.h"
+#include "Student.h"
 
 template <class T>
 void bubbleSort(T *arr);    //Need to pass a pointer for referencing the array
@@ -10,21 +13,40 @@ template <class T>
 void insertionSort(T *arr); //Need to pass a pointer for referencing the array
 
 template <class T>
+void mergeSort(T arr[], int left, int right);
+
+template <class T>
 void quickSort(T *arr, int low, int high);
 
 template <class T>
-T* countingSort(T arr[], int size);
+T* countingSort(T arr[]);
 
 template <class T>
 void radixSort(T arr[]);
+
+Student createRandomStudent();
 
 typedef std::chrono::high_resolution_clock Clock;
 
 int main() {
 
-auto t1 = Clock::now();
-auto t2 = Clock::now();
-std::cout << "Delta t2-t1: " << std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count() << " nanoseconds" << std::endl;
+    auto t1 = Clock::now();
+    auto t2 = Clock::now();
+    std::cout << "Delta t2-t1: " << std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count() << " nanoseconds" << std::endl;
 
-return 0;
+    return 0;
+}
+
+Student createRandomStudent() {
+    std::string firstNames[] = {"Steve", "Sue", "Mark", "Sarah", "Josh", "Kelly", "Jerry", "Lisa", "Paul", "Rachel"};
+    std::string lastNames[] = {"Smith", "Harvey", "North", "Bines", "O'Conner", "Lincoln", "Adams", "Johnson", "Grant", "Trump"};
+    std::string majors[] = {"CS", "Biology", "Law", "Art", "Architecture", "Business", "Engineering", "English", "Gamer", "Doctor"};
+    
+    int randFName = rand() % 10;
+    int randLName = rand() % 10;
+    int randMNum = (rand() % 1000000) +10000000;
+    int randMaj = rand() % 10;
+    
+    Student temp(firstNames[randFName], lastNames[randLName], randMNum, majors[randMaj]);
+    return temp;
 }
--- SortingAlgorithms.h
+++ SortingAlgorithms.h
@@ -1,3 +1,6 @@
+#ifndef __SORTINGALGORITHMS__
+#define __SORTINGALGORITHMS__
+
 
 //Bubble Sort
 template <class T>
@@ -30,7 +33,56 @@ void insertionSort(T *arr){
 	}
 }
 
-//QuickSort
+//Merge Sort
+template <typename T>
+void merge(T arr[], int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    T* L = new T[n1];
+    T* R = new T[n2];
+
+    for (int i = 0; i < n1; ++i)
+        L[i] = arr[left + i];
+    for (int j = 0; j < n2; ++j)
+        R[j] = arr[mid + 1 + j];
+
+    int i = 0, j = 0, k = left;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k++] = L[i++];
+        } else {
+            arr[k++] = R[j++];
+        }
+    }
+
+    while (i < n1) {
+        arr[k++] = L[i++];
+    }
+
+    while (j < n2) {
+        arr[k++] = R[j++];
+    }
+
+    delete[] L;
+    delete[] R;
+}
+
+template <typename T>
+void mergeSort(T arr[], int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+    }
+}
+
+
+//Quick Sort
 template <class T>
 int partition(T *arr, int low, int high) {
 	int pivot = arr[high];
@@ -64,8 +116,8 @@ void quickSort(T *arr, int low, int high) {
 
 //Counting Sort
 template <class T>
-T* countingSort(T arr[], int size) { //may need to be changed to a pointer depending on data size
-    T c[] = new T[size];
+T* countingSort(T arr[]) { //may need to be changed to a pointer depending on data size
+    T c[] = new T[sizeof(arr)];
     for (int i = 0; i < arr.length(); i++) { //gives us our count for each value 
         c[arr[i]]++;
     }
@@ -110,4 +162,4 @@ void radixSort(T arr[]) {
     }
 }
 
-    
+#endif
